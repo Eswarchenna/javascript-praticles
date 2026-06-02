d3ac206af8f645dcbdda4a8690c9496324a3e184
Eswarchenna <chennaeshwar7@gmail.com>
2026-06-02 18:36:50 +0530

    Add files via upload

---
+++ NumberOfFive.js
@@ -0,0 +1,8 @@
+function NumberOfFive(){
+    debugger;
+    var inputNumber = Number(document.getElementById("txtInputNumber").value);
+    var number = inputNumber*5;
+    alert(number);
+
+    
+}
